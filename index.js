b96c51a9b6184001dee36aff44e63c306ec9c779
Christoph Gruener <christophgruener@me.com>
2019-05-29 23:18:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  return document.querySelector('div.grand-node div div div div')
+  return document.querySelector('div#grand-node div div div div')
 }
 
 function increaseRankBy(n) {
