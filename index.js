01c252e7bf3b166e6ccce4ae439fa9a0f323bc37
Christoph Gruener <christophgruener@me.com>
2019-05-29 23:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function index(){
+function deepestChild() {
   return document.querySelector('#grand-node div div div div')
 }
